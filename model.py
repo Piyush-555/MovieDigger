ba39118ac4e3a65026a5e1bca0a3da0fc88b2cb5
ARYANSHU VERMA <48057022+aryanshu@users.noreply.github.com>
2019-11-10 21:39:03 +0530

    update model

--- model.py
+++ model.py
@@ -3,22 +3,28 @@ import pandas as pd
 from scipy import sparse
 ratings = pd.read_csv('dataset/ratings.csv')
 movies = pd.read_csv('dataset/movies.csv')
-ratings = pd.merge(movies,ratings).drop(['genres','timestamp'],axis=1)
+ratings = pd.merge(movies,ratings).drop(['timestamp'],axis=1)
 
 
 userRatings = ratings.pivot_table(index=['userId'],columns=['title'],values='rating')
 userRatings = userRatings.dropna(thresh=10, axis=1).fillna(0,axis=1)
 corrMatrix = userRatings.corr(method='pearson')
 
+def get_popularity_csv():
+	userRatings2=ratings.drop("userId",axis=1)
+	userRatings2=userRatings2.groupby(['movieId','genres']).sum().reset_index()
+	userRatings2=userRatings2.sort_values('rating',ascending=False)
+	userRatings2=userRatings2.drop(["rating"],axis=1)
+	userRatings2.to_csv("dataset/popularity.csv")
 
+get_popularity_csv()
+	
 def get_similar(movieId,rating):
 	similar_ratings = corrMatrix[movieId]*(rating-2.5)
 	similar_ratings = similar_ratings.sort_values(ascending=False)
 	return similar_ratings   
 
 def get_recommendations(movie_list):
-
-
 	similar_movies = pd.DataFrame()
 	for Id,rating in movie_list:
 		movie=ratings.loc[ratings.movieId==Id,"title"]
@@ -39,8 +45,15 @@ def get_recommendations(movie_list):
 	
 
 	return ids
-	
 
+def get_popular(num_movies, genre=None):
+	f=pd.read_csv("dataset/popularity.csv")
+	ids=[]
+	f=f[f["genres"].str.contains(genre)].reset_index()
+	f=f.ix[0:num_movies,"movieId"]
+	ids=f.tolist()
+	ids
+	return ids
 
 
 
