a791e0e62092f6e5ba0115087616dc0301a2fb10
ARYANSHU VERMA <48057022+aryanshu@users.noreply.github.com>
2019-11-10 17:03:05 +0530

    update2.0

--- model.py
+++ model.py
@@ -1,34 +1,29 @@
 
-def pre_process():
-	ratings = pd.read_csv('dataset/ratings.csv')
-	movies = pd.read_csv('dataset/movies.csv')
-	ratings = pd.merge(movies,ratings).drop(['genres','timestamp'],axis=1)
+import pandas as pd
+from scipy import sparse
+ratings = pd.read_csv('dataset/ratings.csv')
+movies = pd.read_csv('dataset/movies.csv')
+ratings = pd.merge(movies,ratings).drop(['genres','timestamp'],axis=1)
 
 
-	userRatings = ratings.pivot_table(index=['userId'],columns=['title'],values='rating')
-	userRatings = userRatings.dropna(thresh=10, axis=1).fillna(0,axis=1)
-	corrMatrix = userRatings.corr(method='pearson')
-	return corrMatrix,ratings
+userRatings = ratings.pivot_table(index=['userId'],columns=['title'],values='rating')
+userRatings = userRatings.dropna(thresh=10, axis=1).fillna(0,axis=1)
+corrMatrix = userRatings.corr(method='pearson')
 
-def get_similar(movieId,rating,corrMatrix):
+
+def get_similar(movieId,rating):
 	similar_ratings = corrMatrix[movieId]*(rating-2.5)
 	similar_ratings = similar_ratings.sort_values(ascending=False)
 	return similar_ratings   
 
-def get_recommendations(action_lover):
-	import pandas as pd
+def get_recommendations(movie_list):
 
-	from scipy import sparse
-	print(action_lover)
-	corrMatrix,ratings=pre_process()
-	print(corrMatrix.head())
-	print(ratings.head())
 
 	similar_movies = pd.DataFrame()
-	for Id,rating in action_lover:
+	for Id,rating in movie_list:
 		movie=ratings.loc[ratings.movieId==Id,"title"]
 		movie=movie.iloc[0]
-		similar_movies = similar_movies.append(get_similar(movie,rating,corrMatrix),ignore_index = True)
+		similar_movies = similar_movies.append(get_similar(movie,rating),ignore_index = True)
 
 	similar_movies=similar_movies.sum().sort_values(ascending=False)[0:10]
 	sim=pd.DataFrame(similar_movies)
