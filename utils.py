877c6efcf06b634983ea9bad7222616fb78a06bf
Piyush Raikwar <piyushraikwar555@gmail.com>
2019-11-19 12:38:00 +0530

    Create base for utils

---
+++ utils.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+
+
+#from model import movies as movies_data
+
+
+def get_tmdb_id(movie_id):
+    pass
+
+
+def get_movie_id(tmdb_id):
+    pass
+
+
+def get_movie_name(movie_id):
+    pass
+
+
+def get_zipped(col1, col2):
+    return zip(col1, col2)
+
+
+def get_unziped(zipped):
+    return list((zip(*zipped)))
